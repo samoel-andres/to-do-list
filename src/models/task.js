acab327bd0d5e04496c1bb3aa386daf63e293b1f
sav <aevs.andres.vidal@gmail.com>
2025-01-16 04:23:34 -0600

    a model for tasks has been created

---
+++ src/models/task.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose')
+
+// Define the schema for an individual task
+const taskItemSchema = new mongoose.Schema({
+    task: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    done: {
+        type: Boolean,
+        required: true,
+        default: false,
+    }
+})
+
+// Define the schema for tasks
+const taskSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        uppercase: true,
+        trim: true,
+    },
+    clasification: {
+        type: String,
+        required: true,
+        trim: true,
+        enum: ['Normal', 'Muy importante']
+    },
+    list: [taskItemSchema]
+})
+
+module.exports = mongoose.model('Task', taskSchema)
