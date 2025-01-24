042d879459e4a1eb84b2c266b44808649c6ffd58
sav <aevs.andres.vidal@gmail.com>
2025-01-24 01:04:22 -0600

    file updated

--- public/js/utils/append-elements.js
+++ public/js/utils/append-elements.js
@@ -83,6 +83,7 @@ export function createElement(config) {
     if (config.required) element.required = config.required
     if (config.text) element.innerText = config.text
     if (config.value) element.value = config.value
+    if (config.off) element.disabled = config.off
 
     if (config.dataAttributes) {
         for (const [key, value] of Object.entries(config.dataAttributes)) {
