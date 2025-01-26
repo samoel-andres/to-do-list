91b679cff48df3f6d017a890ccb9501f39b4f47c
sav <aevs.andres.vidal@gmail.com>
2025-01-26 02:52:53 -0600

    the script for toggling the modal has been added

---
+++ public/js/utils/modal-toggle.js
@@ -0,0 +1,147 @@
+import { loadTask } from "../data/load-task-modal.js"
+import { removeTaskFromModal } from "./remove-elements.js"
+
+document.addEventListener('DOMContentLoaded', () => {
+    let modal
+    let isAnimating = false
+
+    const modalOverlay = document.getElementById('modal-overlay')
+    const toggleCloseModal = document.querySelectorAll('[name="close-modal"]')
+    const toggleOpenNewTaskModal = document.querySelector('#new-item')
+    const parentToggleUpdateModal = document.getElementById('taskList')
+    const dinamicFieldContainer = ['#dinamicFieldsNewModal', '#dinamicFieldsUpdateModal']
+    const modals = ['#new-task-modal', '#edit-task-modal', '#drop-task-modal']
+    const forms = ['#form-new-task-modal', '#form-update-task-modal', '#form-drop-task-modal']
+
+    /**
+     * Displays the required modal.
+     * @param {String} modalSelector - The identifier of the modal to display.
+     * @returns {void}
+     */
+    function showModal(modalSelector) {
+        if (isAnimating) return
+
+        isAnimating = true
+
+        // Hide all displayed modals
+        modals.forEach(modalId => {
+            const modalElement = document.querySelector(modalId)
+            if (modalElement) {
+                modalElement.style.display = 'none'
+                modalElement.classList.remove('active')
+            }
+        })
+
+        modal = document.querySelector(modalSelector)
+
+        if (modal) {
+            modal.style.display = 'flex'
+            modalOverlay.classList.add('active')
+
+            // Animate the modal
+            setTimeout(() => {
+                modal.classList.add('active')
+                isAnimating = false
+            }, 50)
+        }
+    }
+
+    /**
+     * Removes the dynamic fields and resets the form values.
+     * @returns {void}
+     */
+    function cleanFormModal() {
+        // Remove the dynamic fields
+        dinamicFieldContainer.forEach(containerId => {
+            const fieldsContainer = document.querySelector(containerId)
+            if (fieldsContainer) {
+                while (fieldsContainer.firstChild) {
+                    fieldsContainer.removeChild(fieldsContainer.firstChild)
+                }
+            }
+        })
+
+        // Reset the form values
+        forms.forEach(formId => {
+            const resetForm = document.querySelector(formId)
+            if (resetForm) {
+                resetForm.reset()
+            }
+        })
+    }
+
+    // Find all elements and add the function to hide the active modal.
+    toggleCloseModal.forEach(toggle => {
+        toggle.addEventListener('click', () => {
+            if (modal) {
+                modal.classList.remove('active')
+
+                // Animate the modal
+                setTimeout(() => {
+                    modalOverlay.classList.remove('active')
+                    modal = null
+                    cleanFormModal()
+                    removeTaskFromModal('fields-drop-modal')
+                    removeTaskFromModal('fields-update-modal')
+                }, 200)
+            }
+        })
+    })
+
+    // Hide the active modal when the overlay is clicked.
+    modalOverlay.addEventListener('click', (event) => {
+        if (event.target === modalOverlay && modal) {
+            modal.classList.remove('active')
+
+            // Animate the modal
+            setTimeout(() => {
+                modalOverlay.classList.remove('active')
+                modal = null
+                cleanFormModal()
+                removeTaskFromModal('fields-drop-modal')
+                removeTaskFromModal('fields-update-modal')
+            }, 200)
+        }
+    })
+
+    // Hide the active modal when the 'ESC' key is pressed.
+    document.addEventListener('keydown', (event) => {
+        if (event.key === 'Escape' && modal) {
+            modal.classList.remove('active')
+
+            // Animate the modal
+            setTimeout(() => {
+                modalOverlay.classList.remove('active')
+                modal = null
+                cleanFormModal()
+                removeTaskFromModal('fields-drop-modal')
+                removeTaskFromModal('fields-update-modal')
+            }, 200)
+        }
+    })
+
+    // Open the modal to update or delete an activity.
+    parentToggleUpdateModal.addEventListener('click', (event) => {
+        const parentElement = event.target.parentNode
+        if (parentElement) {
+            const id = parentElement.id
+            const dataId = parentElement.getAttribute('data-id')
+            if (id && dataId) {
+                if (id === 'update-item') {
+                    loadTask(dataId, 'update-input-group', 'update-title-task', 'update-clasification', 'fields-update-modal', false)
+                    showModal('#edit-task-modal')
+                } else if (id === 'drop-item') {
+                    loadTask(dataId, 'drop-input-group', 'drop-title-task', 'drop-clasification', 'fields-drop-modal', true)
+                    showModal('#drop-task-modal')
+                }
+            }
+        }
+    })
+
+    // Open the modal to create a new activity.
+    if (toggleOpenNewTaskModal) {
+        toggleOpenNewTaskModal.addEventListener('click', () => {
+            showModal('#new-task-modal')
+        })
+    }
+})
