73a904a83712ba0f400dd77b346d03ea296050f2
sav <aevs.andres.vidal@gmail.com>
2025-02-06 02:10:30 -0600

    file updated

--- public/js/data/load-tasks.js
+++ public/js/data/load-tasks.js
@@ -1,116 +1,122 @@
 import { appendElement } from "../utils/append-elements.js"
+import { showModal } from "../utils/modal-toggle.js"
 
 document.addEventListener('DOMContentLoaded', () => {
+    const modalTitle = document.getElementById('title-alert-modal')
+    const modalMessage = document.getElementById('message-alert-modal')
 
-    /**
-     * Counts the task in the given sub-task list.
-     * @param {Array<Object>} list - The sub-task list to count.
-     * @returns {String} The total number of task in format done/total items.
-     */
-    function countTask(list) {
-        let all = 0
-        let done = 0
-        if (list) {
-            all = list.length
-            list.forEach(item => {
-                if (item.done) {
-                    done++
+    loadTaskData(modalTitle, modalMessage)
+})
+
+/**
+ * Counts the task in the given sub-task list.
+ * @param {Array<Object>} list - The sub-task list to count.
+ * @returns {String} The total number of task in format done/total items.
+ */
+function countTask(list) {
+    let all = 0
+    let done = 0
+    if (list) {
+        all = list.length
+        list.forEach(item => {
+            if (item.done) {
+                done++
+            }
+        })
+    }
+
+    return `${done}/${all}`
+}
+
+/**
+ * Prepares an array of configuration objects to create and add elements to the DOM.
+ * @param {Array<Object>} data - The list of tasks retrieved from the database.
+ * @returns {Array<Object>} An array of configuration objects to be used for creating and adding elements to the DOM.
+ */
+function prepareConfigurationObject(data) {
+    let objectConfig = []
+
+    if (data) {
+        data.forEach((item, index) => {
+            objectConfig.push({
+                element: {
+                    element: 'tr',
+                    children: [
+                        {
+                            element: 'td',
+                            id: item._id,
+                            text: index + 1,
+                        },
+                        {
+                            element: 'td',
+                            text: item.title.toUpperCase(),
+                        },
+                        {
+                            element: 'td',
+                            text: countTask(item.list),
+                        }
+                        ,
+                        {
+                            element: 'td',
+                            text: item.clasification,
+                        },
+                        {
+                            element: 'td',
+                            class: 'display-flex h-around',
+                            children: [
+                                {
+                                    element: 'a',
+                                    class: 'border-radius-2',
+                                    id: 'update-item',
+                                    name: 'update-item',
+                                    dataAttributes: { id: item._id },
+                                    children: [
+                                        {
+                                            element: 'i',
+                                            class: 'bi bi-pen fs-2',
+                                        }
+                                    ]
+                                },
+                                {
+                                    element: 'a',
+                                    class: 'border-radius-2',
+                                    id: 'drop-item',
+                                    name: 'drop-item',
+                                    dataAttributes: { id: item._id },
+                                    children: [
+                                        {
+                                            element: 'i',
+                                            class: 'bi bi-trash fs-2',
+                                        }
+                                    ]
+                                }
+                            ]
+                        }
+                    ]
                 }
             })
-        }
-
-        return `${done}/${all}`
+        })
     }
 
-    /**
-     * Prepares an array of configuration objects to create and add elements to the DOM.
-     * @param {Array<Object>} data - The list of tasks retrieved from the database.
-     * @returns {Array<Object>} An array of configuration objects to be used for creating and adding elements to the DOM.
-     */
-    function prepareConfigurationObject(data) {
-        let objectConfig = []
+    return objectConfig
+}
 
-        if (data) {
-            data.forEach((item, index) => {
-                objectConfig.push({
-                    element: {
-                        element: 'tr',
-                        children: [
-                            {
-                                element: 'td',
-                                id: item._id,
-                                text: index + 1,
-                            },
-                            {
-                                element: 'td',
-                                text: item.title.toUpperCase(),
-                            },
-                            {
-                                element: 'td',
-                                text: countTask(item.list),
-                            }
-                            ,
-                            {
-                                element: 'td',
-                                text: item.clasification,
-                            },
-                            {
-                                element: 'td',
-                                class: 'display-flex h-around',
-                                children: [
-                                    {
-                                        element: 'a',
-                                        class: 'border-radius-2',
-                                        id: 'update-item',
-                                        name: 'update-item',
-                                        dataAttributes: { id: item._id },
-                                        children: [
-                                            {
-                                                element: 'i',
-                                                class: 'bi bi-pen fs-3',
-                                            }
-                                        ]
-                                    },
-                                    {
-                                        element: 'a',
-                                        class: 'border-radius-2',
-                                        id: 'drop-item',
-                                        name: 'drop-item',
-                                        dataAttributes: { id: item._id },
-                                        children: [
-                                            {
-                                                element: 'i',
-                                                class: 'bi bi-trash fs-3',
-                                            }
-                                        ]
-                                    }
-                                ]
-                            }
-                        ]
-                    }
-                })
-            })
-        }
+/**
+ * Loads the data into the table.
+ * @returns {Array<Object>} An array objects containing the data retrieved from the database.
+ */
+export async function loadTaskData(modalTitle, modalMessage) {
+    const data = await fetchTaskData()
 
-        return objectConfig
+    if (data) {
+        const objectConfig = prepareConfigurationObject(data)
+        objectConfig.forEach(objectConfig => appendElement(objectConfig.element, 'taskList'))
+    } else {
+        modalTitle.innerHTML = 'Sin registros'
+        modalMessage.innerHTML = 'Actualmente no hay tareas registradas. Comience agregando una para gestionarlas mejor.'
+        showModal('#alert-modal')
     }
-
-    /**
-     * Loads the data into the table.
-     * @returns {Array<Object>} An array objects containing the data retrieved from the database.
-     */
-    async function logTaskData() {
-        const data = await fetchTaskData()
-        if (data) {
-            const objectConfig = prepareConfigurationObject(data)
-            objectConfig.forEach(objectConfig => appendElement(objectConfig.element, 'taskList'))
-        } else {
-            console.log('No data found')
-        }
-    }
-
-    logTaskData()
-})
+}
 
 /**
  * Fetches the list of documents from the server and processes the data.
@@ -122,6 +128,8 @@ document.addEventListener('DOMContentLoaded', () => {
 export async function fetchTaskData() {
     try {
         const res = await axios.get('http://localhost:3001/find/documents/all')
+        if (!res.data) return
+
         const { data, read, message } = res.data
 
         if (read) {
